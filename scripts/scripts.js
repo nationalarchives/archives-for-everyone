1b34d3515f5c6f9b065609f9b625a2d5000c5f23
Gwyn Jones <m@gwnjns.com>
2019-05-28 21:00:13 +0100

    Refactor JS animations to reduce repetition in code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,48 +18,28 @@ const createObserver = (element, className) => {
 
 window.addEventListener("load", function (event) {
 
-    // Animate text over the map
-    const map_lead = document.querySelector('.map-container .lead');
-    createObserver(map_lead, "slide-from-left");
+    const animations = [
+        { selector: '.map-container .lead', className: 'slide-from-left' },
+        { selector: '.part-two h2', className: 'fade-in' },
+        { selector: '.part-two h2', className: 'fade-in' },
+        { selector: '.part-four h2', className: 'fade-in' },
+        { selector: '.part-five h2', className: 'fade-in' },
+        { selector: '.we-must-be ul li:nth-child(1)', className: 'slide-from-left' },
+        { selector: '.we-must-be ul li:nth-child(2)', className: 'slide-from-right' },
+        { selector: '.we-must-be ul li:nth-child(3)', className: 'slide-from-left' },
+        { selector: '#inclusive-archive h3', className: 'fade-in' },
+        { selector: '#inclusive-archive p', className: 'slide-from-right' },
+        { selector: '#entrepreneurial-archive h3', className: 'fade-in' },
+        { selector: '#entrepreneurial-archive p', className: 'slide-from-right' },
+        { selector: '#disruptive-archive h3', className: 'fade-in' },
+        { selector: '#disruptive-archive p', className: 'slide-from-right' }
 
-    // Animate the h2 in .part-two
-    const part_two_h2 = document.querySelector('.part-two h2');
-    createObserver(part_two_h2, "fade-in");
+    ];
 
-    // Animate the h2 in .part-four
-    const part_four_h2 = document.querySelector('.part-four h2');
-    createObserver(part_four_h2, "fade-in");
-
-    // Animate the h2 in .part-four
-    const part_five_h2 = document.querySelector('.part-five h2');
-    createObserver(part_five_h2, "fade-in");
-
-    // Animate the 'we must be' list items
-    const li1 = document.querySelector('.we-must-be ul li:nth-child(1)');
-    const li2 = document.querySelector('.we-must-be ul li:nth-child(2)');
-    const li3 = document.querySelector('.we-must-be ul li:nth-child(3)');
-    createObserver(li1, "slide-from-left");
-    createObserver(li2, "slide-from-right");
-    createObserver(li3, "slide-from-left")
-
-    const inclusive_archive_heading = document.querySelector('#inclusive-archive h3');
-    createObserver(inclusive_archive_heading, "fade-in");
-
-    const inclusive_archive_p = document.querySelector('#inclusive-archive p');
-    createObserver(inclusive_archive_p, "slide-from-right");
-
-    const entrepreneurial_archive_heading = document.querySelector('#entrepreneurial-archive h3');
-    createObserver(entrepreneurial_archive_heading, "fade-in");
-
-    const entrepreneurial_archive_p = document.querySelector('#entrepreneurial-archive p');
-    createObserver(entrepreneurial_archive_p, "slide-from-right");
-
-    const disruptive_archive_heading = document.querySelector('#disruptive-archive h3');
-    createObserver(disruptive_archive_heading, "fade-in");
-
-    const disruptive_archive_p = document.querySelector('#disruptive-archive p');
-    createObserver(disruptive_archive_p, "slide-from-left");
-
+    animations.forEach(function (item) {
+        const entrepreneurial_archive_p = document.querySelector(item.selector);
+        createObserver(entrepreneurial_archive_p, item.className);
+    });
 
 
 }, false);
