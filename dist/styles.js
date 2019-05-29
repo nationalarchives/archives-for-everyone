fbe5377c2a7091220ed6b796b83de2148447b6b5
Gwyn Jones <m@gwnjns.com>
2019-05-29 11:15:01 +0100

    Fix issue with paragraph on smaller screens

--- dist/styles.js
+++ dist/styles.js
@@ -117,84 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"scripts/scripts.js":[function(require,module,exports) {
-var toggle_class_on_intersection = function toggle_class_on_intersection(entries, element, className) {
-  if (entries[0].isIntersecting) {
-    element.classList.add(className);
-  } else {
-    element.classList.remove(className);
+})({"../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
-};
 
-var createObserver = function createObserver(element, className) {
-  var observer = new IntersectionObserver(function (iObserver) {
-    toggle_class_on_intersection(iObserver, element, className);
-  }, {
-    root: null,
-    rootMargin: "0px"
-  });
-  observer.observe(element);
-};
+  return bundleURL;
+}
 
-window.addEventListener("load", function (event) {
-  var animations = [{
-    selector: '.map-container .lead',
-    className: 'slide-from-left'
-  }, {
-    selector: '.part-two h2',
-    className: 'fade-in'
-  }, {
-    selector: '.part-two h2',
-    className: 'fade-in'
-  }, {
-    selector: '.part-four h2',
-    className: 'fade-in'
-  }, {
-    selector: '.part-five h2',
-    className: 'fade-in'
-  }, {
-    selector: '.we-must-be ul li:nth-child(1)',
-    className: 'slide-from-left'
-  }, {
-    selector: '.we-must-be ul li:nth-child(2)',
-    className: 'slide-from-right'
-  }, {
-    selector: '.we-must-be ul li:nth-child(3)',
-    className: 'slide-from-left'
-  }, {
-    selector: '#inclusive-archive h3',
-    className: 'fade-in'
-  }, {
-    selector: '#inclusive-archive p',
-    className: 'slide-from-right'
-  }, {
-    selector: '#entrepreneurial-archive h3',
-    className: 'fade-in'
-  }, {
-    selector: '#entrepreneurial-archive p',
-    className: 'slide-from-right'
-  }, {
-    selector: '#disruptive-archive h3',
-    className: 'fade-in'
-  }, {
-    selector: '#disruptive-archive p',
-    className: 'slide-from-left'
-  }, {
-    selector: '.part-five p:nth-child(1)',
-    className: 'slide-from-right'
-  }, {
-    selector: '.part-five p:nth-child(2)',
-    className: 'slide-from-left'
-  }, {
-    selector: '.part-five p:nth-child(3)',
-    className: 'fade-in'
-  }];
-  animations.forEach(function (item) {
-    var entrepreneurial_archive_p = document.querySelector(item.selector);
-    createObserver(entrepreneurial_archive_p, item.className);
-  });
-}, false);
-},{}],"../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -222,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63200" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56604" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -397,5 +392,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","scripts/scripts.js"], null)
-//# sourceMappingURL=/scripts.d2cda223.js.map
+},{}]},{},["../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/styles.js.map
