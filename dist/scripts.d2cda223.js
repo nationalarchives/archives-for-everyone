6469721deebb400c0ac15f3932217225cd41a418
Matt Blair <mdrblair@gmail.com>
2019-06-04 13:54:17 +0100

    - Fixing logo spacing issue in masthead

--- dist/scripts.d2cda223.js
+++ dist/scripts.d2cda223.js
@@ -188,7 +188,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64926" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50638" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
